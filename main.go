4d5b1927c30aca37252077764a6ef8606a72c449
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-01-25 02:02:45 +0000

    yolo

--- main.go
+++ main.go
@@ -1,56 +1,88 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/alexstoick/wow/database"
+	"github.com/alexstoick/wow/models"
+	"github.com/jinzhu/gorm"
+	"github.com/pquerna/ffjson/ffjson"
 	"io/ioutil"
+	"net/http"
 	"time"
 )
 
-type JSONFile struct {
-	Realms   []Realm
-	Auctions []Auction
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
-type Realm struct {
-	Name string
-	Slug string
+
+func SaveAuction(auction models.Auction) {
+	db := database.ConnectToDb()
+	db.Create(&auction)
 }
-type Auction struct {
-	ID           int `json:"auc"`
-	Item_id      int `json:"item"`
-	Owner        string
-	OwnerRealm   string
-	Bid          int
-	Buyout       int
-	Quantity     int
-	TimeLeft     string
-	Rand         int
-	Seed         int
-	Context      int
-	BonusList    []Bonus `json:"bonusLists"`
-	Modifiers    []Modifier
-	PetSpeciesId int
-	PetBreedId   int
-	PetLevel     int
-	PetQualityId int
+
+func ProcessAuctions(ah_file models.AHFile) {
+
+	t0 := time.Now()
+	resp, _ := http.Get(ah_file.URL)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	t1 := time.Now()
+	fmt.Printf("The call to DOWNLOAD AH FILE took %v to run.\n", t1.Sub(t0))
+	fmt.Printf("ah_file ID %d\n", ah_file.ID)
+
+	type JSONFile struct {
+		Realms   []models.Realm
+		Auctions []models.Auction
+	}
+
+	var jsonfile JSONFile
+	t0 = time.Now()
+	ffjson.Unmarshal(body, &jsonfile)
+
+	//concurrency := 10
+	//sem := make(chan bool, concurrency)
+
+	for i := 0; i < 2; i++ { //i < len(jsonfile.Auctions); i++ { //
+		auction := jsonfile.Auctions[i]
+		auction.ImportedFromId = ah_file.ID
+		SaveAuction(auction)
+	}
+	t1 = time.Now()
+	fmt.Printf("The call to SAVE AUCTIONS took %v to run.\n", t1.Sub(t0))
 }
-type Modifier struct {
-	Type  int
-	Value int
-}
-type Bonus struct {
-	Id int `json:"bonusListId"`
+
+var db gorm.DB
+
+func GetLatestAHFilelist() models.AHFile {
+
+	t0 := time.Now()
+	resp, _ := http.Get("https://eu.api.battle.net/wow/auction/data/quelthalas?locale=en_GB&apikey=5kuxc3d7rjwk75dvds22egepcwajwtqx")
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	t1 := time.Now()
+	fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
+
+	type DataDump struct {
+		Files []models.AHFile
+	}
+	var file DataDump
+	ffjson.Unmarshal(body, &file)
+	fmt.Printf("%+v\n", file)
+	fmt.Printf("%+v\n", file.Files[0].URL)
+	ahfile := file.Files[0]
+	db.Create(&ahfile)
+	return ahfile
 }
 
 func main() {
-	fmt.Printf("lol\n")
-	file, _ := ioutil.ReadFile("./auctions.json")
 
-	var jsonfile JSONFile
-	t0 := time.Now()
-	json.Unmarshal(file, &jsonfile)
-	t1 := time.Now()
-	fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
-	//fmt.Printf("res: %+v\n", jsonfile)
+	db := database.ConnectToDb()
+	database.AutoMigrateModels(db)
 
+	//ah_file := GetLatestAHFilelist()
+	var ah_file models.AHFile
+	db.Last(&ah_file)
+	ProcessAuctions(ah_file)
 }
---
+++ models/models.go
@@ -0,0 +1,90 @@
+package models
+
+import "time"
+
+type Realm struct {
+	Name string
+	Slug string
+}
+type Auction struct {
+	ID             int `gorm:"primary_key"`
+	AuctionID      int `json:"auc" sql:"type:bigint"`
+	Item_id        int `json:"item" sql:"index:auction_item_id_index;type:bigint"`
+	Owner          string
+	OwnerRealm     string
+	Bid            int `sql:"type:bigint"`
+	Buyout         int `sql:"type:bigint"`
+	Quantity       int
+	TimeLeft       string
+	Rand           int
+	Seed           int
+	Context        int
+	BonusList      []Bonus `json:"bonusLists"`
+	Modifiers      []Modifier
+	PetSpeciesId   int
+	PetBreedId     int
+	PetLevel       int
+	PetQualityId   int
+	ImportedFrom   AHFile
+	ImportedFromId int       `json:"-" sql:"index:auction_imported_from_index"`
+	ImportedAt     time.Time `sql:"index:auction_imported_at_index"`
+}
+type Modifier struct {
+	Type  int
+	Value int
+}
+type Bonus struct {
+	Id int `json:"bonusListId"`
+}
+
+type ItemMaterial struct {
+	ID         int `gorm:"primary_key"`
+	ItemID     int `sql:"unique_index:item_and_material"`
+	MaterialID int `sql:"unique_index:item_and_material"`
+	Quantity   int
+}
+
+type Item struct {
+	ItemID         int    `gorm:"primary_key"`
+	ItemName       string `sql:"index:items_item_name_index"`
+	InBlizzardAPI  int
+	InWowheadAPI   int
+	IsAuctionable  int
+	GlobalMedianEU int
+	Alchemy        int
+	Archaeology    int
+	Blacksmithing  int
+	Cooking        int
+	Disenchanting  int
+	Enchanting     int
+	Engineering    int
+	Firstaid       int
+	Herbalism      int
+	Inscription    int
+	Jewelcrafting  int
+	Leatherworking int
+	Milling        int
+	Mining         int
+	Prospecting    int
+	Skinning       int
+	Tailoring      int
+	StackSize      int
+	BuyPrice       int `sql:"type:bigint"`
+	SellPrice      int `sql:"type:bigint"`
+	ItemClass      string
+	ItemSubClass   string
+	ItemType       string
+	InventoryType  string
+	Equippable     int
+	Source         string
+	SourceId       int
+	SourceType     string
+	SourceDesc     string
+	DeprecatedMsg  string
+}
+type AHFile struct {
+	ID           int    `gorm:"primary_key"`
+	URL          string `sql:"not_null"`
+	LastModified int    `sql:"type:bigint;unique"`
+	CreatedAt    time.Time
+}
