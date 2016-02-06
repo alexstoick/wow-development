4294193558b41abb786d23eaf7edae9d02b447b4
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-02-06 01:33:06 +0000

    godep 2

---
+++ datafetch/Godeps/_workspace/src/github.com/alexstoick/wow/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"fmt"
+	"github.com/alexstoick/wow/datafetch/Godeps/_workspace/src/github.com/alexstoick/wow/models"
+	"github.com/alexstoick/wow/datafetch/Godeps/_workspace/src/github.com/jinzhu/gorm"
+	"github.com/alexstoick/wow/datafetch/Godeps/_workspace/src/github.com/lib/pq"
+	"time"
+)
+
+func AutoMigrateModels(db gorm.DB) {
+	t0 := time.Now()
+	db.AutoMigrate(&models.Item{})
+	db.AutoMigrate(&models.ItemMaterial{})
+	db.AutoMigrate(&models.AHFile{})
+	db.AutoMigrate(&models.Auction{})
+	t1 := time.Now()
+	fmt.Printf("The DB MIGRATE call took %v to run.\n", t1.Sub(t0))
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func ConnectToDb() (db gorm.DB) {
+	const (
+		DB_USER     = ""
+		DB_PASSWORD = ""
+		DB_NAME     = "wow_development"
+	)
+	dbinfo := fmt.Sprintf("host=postgres user=postgres dbname=%s sslmode=disable", DB_NAME)
+	pq.ParseURL(dbinfo)
+	var err error
+
+	db, err = gorm.Open("postgres", dbinfo)
+	handleError(err)
+	return db
+}
