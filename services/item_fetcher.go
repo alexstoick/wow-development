462efb87195eaad2a5101323473941da8e9ec9cd
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-03-28 15:40:22 +0100

    now runs

--- services/item_fetcher.go
+++ services/item_fetcher.go
@@ -1,25 +1,23 @@
 package services
 
 import (
-	"fmt"
 	"github.com/alexstoick/wow/models"
 	"github.com/jinzhu/gorm"
-	"time"
 )
 
 type ItemFetcher struct {
 	Db gorm.DB
 }
 
-func (fetcher ItemFetcher) LoadItem(id string) (item models.Item, err Error) {
+func (fetcher ItemFetcher) LoadItem(id string) (item models.Item, err error) {
 	db := fetcher.Db
 
-	err := db.Find(&item, id).Error
+	err = db.Find(&item, id).Error
 
 	return item, err
 }
 
-func (fetcher ItemFetcher) LoadAuctions() (item models.Item, err Error) {
+func (fetcher ItemFetcher) LoadAuctions() (item models.Item, err error) {
 	db := fetcher.Db
 	err = db.Preload("Auctions", func(db *gorm.DB) *gorm.DB {
 		return db.Where("present = ?", true).Where("buyout > 0").Order("(auctions.buyout/auctions.quantity), auctions.imported_at DESC")
@@ -28,7 +26,7 @@ func (fetcher ItemFetcher) LoadAuctions() (item models.Item, err Error) {
 	return item, err
 }
 
-func (fetcher ItemFetcher) LoadSpells() (item models.Item, err Error) {
+func (fetcher ItemFetcher) LoadSpells() (item models.Item, err error) {
 	db := fetcher.Db
 	err = db.Preload("Spells").Preload("Spells.ItemMaterials").Preload("Spells.ItemMaterials.Material").Find(&item).Error
 
