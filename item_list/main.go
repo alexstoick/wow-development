8cde91c1b7756266e19ddba066ea8636a31c9a26
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-04-04 19:44:03 +0100

    item list micro

---
+++ item_list/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/gin-gonic/gin"
+	"gopkg.in/redis.v3"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		//Addr:     "localhost:6379",
+		Addr:     "redis:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+}
